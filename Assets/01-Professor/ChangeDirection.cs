b034019f271d3380667b6cd07a82b5cfd3b0383e
Sunny <the.sunny.cheng@gmail.com>
2025-11-12 12:53:15 -0500

    Code and Animation
    Bouncing ball and animating sprite with keyboard inputs
    -set ints to directions
    -code to use ints to correlate to directional inputs

--- Assets/01-Professor/ChangeDirection.cs
+++ Assets/01-Professor/ChangeDirection.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ChangeDirection : MonoBehaviour
 {
+
+    private Animator anim;
+    private float horixontalMove;
+    private float verticalMove;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horixontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        //Horizontal move
+        if (horixontalMove < 0)
+        {
+            anim.SetInteger("Direction", 3);
+        }
+        if (horixontalMove > 0)
+        {
+            anim.SetInteger("Direction", 1);
+        }
+
+        //Vertical move
+        if (verticalMove < 0)
+        {
+            anim.SetInteger("Direction", 2);
+        }
+        if (verticalMove > 0)
+        {
+            anim.SetInteger("Direction", 0);
+        }
     }
 }
